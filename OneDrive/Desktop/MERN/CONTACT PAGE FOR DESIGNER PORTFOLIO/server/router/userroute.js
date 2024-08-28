fd6b8eda3451f9af544c8ddfe2329ee771c658ca
Turbobot <turbobot@vercel.com>
2024-08-28 14:27:48 +0530

    feat(create-turbo): apply official-starter transform

--- OneDrive/Desktop/MERN/CONTACT PAGE FOR DESIGNER PORTFOLIO/server/router/userroute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const {  sendMail, registerUser} = require("../controllers/usercontroller");
-const router = express.Router();
-
-
-router.route('/registerdata').post(registerUser);
-router.route('/sendMail').post(registerUser,sendMail);
-
-module.exports = router;
